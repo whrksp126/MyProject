b58e6b51c102f5d31783362f0e0387743a5daa20
whrksp126 <whrksp126@naver.com>
2021-09-14 13:05:23 +0900

    main page design

---
+++ material-ui-test-project/src/components/ItemList.js
@@ -0,0 +1,373 @@
+import React from 'react'
+import Box from '@material-ui/core/Box';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+function ItemList() {
+  return (
+    <>
+      {/* Recomend Item */}
+      <Box display="flex" flexWrap="wrap" justifyContent="space-between">
+
+        {/* item */}
+        <Box pb={5}>
+          <Box style={{ position: "relative" }}>
+            <Box
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://dnvefa72aowie.cloudfront.net/origin/article/202010/d82700fde3034433766c2b9a2ff71d3f4512e9424601dd3a757abfa67bb5bfb3.webp?q=95&s=1440x1440&t=inside")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",  top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://media.bunjang.co.kr/product/146877743_3_1613909519_w360.jpg")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",  top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://vinclo2.cafe24.com/web/upload/NNEditor/2021/0823/B01230%20(1).JPG")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",   top:"20px",  left:"20px", fontSize: 40,  color:"#FF7D9E" }} />
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGyW7VJTHVEGDYyvGj08GSzH4MrYTGp3zrsw&usqp=CAU")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",  top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2LImHHW0Rb6Xbv2VkQI1bxFD88-hc9FasYQ&usqp=CAU")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",  top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://i.pinimg.com/736x/f5/02/88/f5028873fdc6f9d5f1ed606b7bd3cb1b.jpg")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",   top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://static.coupangcdn.com/image/vendor_inventory/c615/444066c1d8796467022dffdef9d6fe2d239837064e4d4e55f0ebd6cc167c.jpg")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",   top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("https://m.vintage90.co.kr/web/product/medium/24778.jpg")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",   top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("http://m.vintage-collection.co.kr/web/product/medium/vc-10531.jpg")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",  top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+
+        {/* item */}
+        <Box pb={5}>
+        <Box style={{ position: "relative" }}>
+            <Box 
+              width="253px" 
+              height="253px" 
+              borderRadius={25}
+              style={{ 
+                backgroundImage: `url("http://ccimg.hellomarket.com/images/2018/item/11/04/00/3543_2037744_1.jpg?size=s6")`,
+                backgroundSize: '100% 100%',
+                backgroundRepeat: 'no-repeat',
+              }}
+            >
+            <FavoriteBorderIcon style={{ position: "absolute",   top:"20px",  left:"20px",  fontSize: 40,  color:"#FF7D9E" }} />              
+            </Box>  
+            <Box 
+              width="253px" 
+              marginTop="20px"
+              fontSize={17}
+            >슬림 골지 폴라니트2종(T)
+            </Box>  
+            <Box  marginTop="10px" fontSize={23} mb={1}>
+              <Box color="#ff485a" display="inline">38%</Box>
+              <Box display="inline">10,000</Box>
+            </Box>  
+            <Box 
+              width="253px" 
+              textOverflow="ellipsis"
+              overflow="hidden"
+              whiteSpace="nowrap"
+              color="#ff485a"
+            >주말특가♥ 딱 3일만 인기상품 세일! 13일 17:00 까지!</Box>  
+          </Box>
+        </Box>
+
+
+      </Box>
+    </>
+  )
+}
+
+export default ItemList
